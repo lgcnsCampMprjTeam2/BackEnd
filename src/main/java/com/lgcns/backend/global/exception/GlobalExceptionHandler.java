262f64b8a22c72db9d970d5dbd0f28c991c03698
danajlim <danalim0819@gmail.com>
2025-05-18 11:37:50 +0900

    fix: ExceptionHandler

--- src/main/java/com/lgcns/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lgcns/backend/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lgcns.backend.global.exception;
 
 import com.lgcns.backend.global.code.GeneralErrorCode;
 import com.lgcns.backend.global.response.CustomResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +16,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(CustomResponse.fail(e.getErrorCode(), null));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CustomResponse<?>> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(CustomResponse.fail(GeneralErrorCode._BAD_REQUEST, e.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<CustomResponse<?>> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(CustomResponse.fail(GeneralErrorCode._NOT_FOUND, e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
