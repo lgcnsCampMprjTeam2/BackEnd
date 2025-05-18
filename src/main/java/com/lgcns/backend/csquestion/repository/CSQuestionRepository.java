10de0abfd101f111094cfe4081bf58908e859e5f
sky121016 <sky121016@naver.com>
2025-05-18 16:39:19 +0900

    feat: CS질문 리스트 페이지네이션 구현

--- src/main/java/com/lgcns/backend/csquestion/repository/CSQuestionRepository.java
+++ src/main/java/com/lgcns/backend/csquestion/repository/CSQuestionRepository.java
@@ -1,6 +1,9 @@
 package com.lgcns.backend.csquestion.repository;
 
 import com.lgcns.backend.csquestion.domain.CSQuestion;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import com.lgcns.backend.global.domain.Category;
@@ -9,6 +12,6 @@ import java.time.LocalDateTime;
 
 
 public interface CSQuestionRepository extends JpaRepository<CSQuestion, Long>{
-    List<CSQuestion> findByCategory(Category category);
+    Page<CSQuestion> findByCategory(Category category, Pageable pageable);
     CSQuestion findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
