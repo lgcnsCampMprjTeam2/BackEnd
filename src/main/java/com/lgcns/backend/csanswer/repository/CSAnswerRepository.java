2180df0a39685f3bd3737c7228be1268a8eff4cc
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-05-19 19:25:27 +0900

    feat: add pagination and filtering by question for CSAnswer list
    Modified CSAnswerController, CSAnswerService, and CSAnswerRepository

--- src/main/java/com/lgcns/backend/csanswer/repository/CSAnswerRepository.java
+++ src/main/java/com/lgcns/backend/csanswer/repository/CSAnswerRepository.java
@@ -1,12 +1,17 @@
 package com.lgcns.backend.csanswer.repository;
 
 import com.lgcns.backend.csanswer.domain.CSAnswer;
+import com.lgcns.backend.csquestion.domain.CSQuestion;
 import com.lgcns.backend.user.domain.User;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CSAnswerRepository extends JpaRepository<CSAnswer, Long>{
     List<CSAnswer> findAllByUser(User user);
+    Page<CSAnswer> findAllByUser(User user, Pageable pageable);
+    Page<CSAnswer> findAllByUserAndCsQuestion(User user, CSQuestion csQuestion, Pageable pageable);
 }
